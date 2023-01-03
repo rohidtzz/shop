884a95f7f55319cf615bde16f2d5d3a6765d4b21
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2023-01-03 19:58:25 +0700

    commit fixing

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -16,35 +16,35 @@ class AddressSeeder extends Seeder
      */
     public function run()
     {
-        Address::create([
-            'city_id' => 2,
-            'province_id' => 21,
-            'alamat' => "alamat apa aja",
-            'user_id' => 1,
-            'kode_pos' => "27651",
-            'no_hp' => "08125861",
-            'name' => "admin"
-        ]);
+        // Address::create([
+        //     'city_id' => 2,
+        //     'province_id' => 21,
+        //     'alamat' => "alamat apa aja",
+        //     'user_id' => 1,
+        //     'kode_pos' => "27651",
+        //     'no_hp' => "08125861",
+        //     'name' => "admin"
+        // ]);
 
-        Address::create([
-            'city_id' => 2,
-            'province_id' => 21,
-            'alamat' => "alamat apa aja",
-            'user_id' => 2,
-            'kode_pos' => "27651",
-            'no_hp' => "08125861",
-            'name' => "staff"
-        ]);
+        // Address::create([
+        //     'city_id' => 2,
+        //     'province_id' => 21,
+        //     'alamat' => "alamat apa aja",
+        //     'user_id' => 2,
+        //     'kode_pos' => "27651",
+        //     'no_hp' => "08125861",
+        //     'name' => "staff"
+        // ]);
 
-        Address::create([
-            'city_id' => 2,
-            'province_id' => 21,
-            'alamat' => "alamat apa aja",
-            'user_id' => 3,
-            'kode_pos' => "27651",
-            'no_hp' => "08125861",
-            'name' => "user"
-        ]);
+        // Address::create([
+        //     'city_id' => 2,
+        //     'province_id' => 21,
+        //     'alamat' => "alamat apa aja",
+        //     'user_id' => 3,
+        //     'kode_pos' => "27651",
+        //     'no_hp' => "08125861",
+        //     'name' => "user"
+        // ]);
 
 
     }
